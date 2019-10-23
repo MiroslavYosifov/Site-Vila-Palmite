23488d0adc8e75f4f25fc64dd8f120154a104ba5
MiroslavYosifov <m.yosifov193@gmail.com>
2019-10-23 03:11:31 +0300

    add mongodb url

--- server/config/settings.js
+++ server/config/settings.js
@@ -1,18 +1,19 @@
 const path = require('path');
 const port = process.env.PORT || 1337;
+const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/database';
 let roothPath = path.normalize(path.resolve(__dirname, '/../../'));
 
 module.exports = {
     development: {
         rootPath: roothPath,
-        db: 'mongodb://localhost:27017/database',
+        db: CONNECTION_URI,
         port: port
     },
     staging: {
     },
     production: {
         rootPath: roothPath,
-        db: 'mongodb://localhost:27017/database',
+        db: CONNECTION_URI,
         port: port
     }
 }
