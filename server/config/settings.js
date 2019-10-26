6e99615ac5b3d1f727faf7e054a2b2fa341da284
MiroslavYosifov <m.yosifov193@gmail.com>
2019-10-26 22:22:50 +0300

    add mongodb url

--- server/config/settings.js
+++ server/config/settings.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const port = process.env.PORT || 1337;
-const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/database';
+const CONNECTION_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/database';
 let roothPath = path.normalize(path.resolve(__dirname, '/../../'));
 
 module.exports = {
