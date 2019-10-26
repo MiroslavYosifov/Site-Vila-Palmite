d4346df6e05ecb07fd76c746bcf32168ef785365
MiroslavYosifov <m.yosifov193@gmail.com>
2019-10-27 00:50:54 +0300

    change URL

--- server/config/settings.js
+++ server/config/settings.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const port = process.env.PORT || 1337;
-const CONNECTION_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/database';
+
+//const CONNECTION_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/database';
+const CONNECTION_URL = 'mongodb://miro:123456abc@sitecluster-shard-00-00-eis0r.mongodb.net:27017,sitecluster-shard-00-01-eis0r.mongodb.net:27017,sitecluster-shard-00-02-eis0r.mongodb.net:27017/test?ssl=true&replicaSet=siteCluster-shard-0&authSource=admin&retryWrites=true&w=majority';
+
 let roothPath = path.normalize(path.resolve(__dirname, '/../../'));
 
 module.exports = {
