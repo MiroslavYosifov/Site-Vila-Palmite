355cb6daacb7fa12656a7f605315c45600ed76d5
MiroslavYosifov <m.yosifov193@gmail.com>
2019-10-27 19:31:18 +0200

    add image scripts

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,3 +1,5 @@
+// Slides
+
 let clickDoctors = 1;
 
 function moveSlides(e){
@@ -23,4 +25,41 @@ function moveSlides(e){
         }
     }
     clickDoctors ++;   
+}
+
+// Gallery
+
+// function showImage (e){
+//     e.preventDefault();
+//     console.log(e.target);
+//     let imgUrl = e.target.src;
+//     console.log(imgUrl);
+//     let modal = document.getElementsByClassName('modal')[0];
+//     let modalUrl = document.getElementsByClassName('inner')[0].childNodes[1].childNodes[0];
+//     modalUrl.src = imgUrl;
+//     modal.style.display = "flex";  
+// }
+
+// function hideImage (e){
+//     let modal = document.getElementsByClassName('modal')[0];
+//     modal.style.display = "none";
+// }
+
+function showHideImage(e) {
+    e.preventDefault();
+    let modal = document.getElementsByClassName('modal')[0];
+    // console.log(modal);
+    // console.log(e.target);
+    
+    if(modal.className === "modal checked" && e.target.className === "modal checked") {
+        modal.style.display = "none";
+        modal.className = "modal";
+
+    } else if (modal.className === "modal"){
+        let imgUrl = e.target.src;
+        let modalUrl = document.getElementsByClassName('inner')[0].childNodes[1].childNodes[0];
+        modalUrl.src = imgUrl;
+        modal.style.display = "flex";
+        modal.className = "modal checked";
+    }
 }
